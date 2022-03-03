3ecc62148200aefb493893f56539a018bc17127b
alexander.a.volkov@rambler.ru <alexander.a.volkov@rambler.ru>
2022-03-03 19:49:25 +0300

    Добавьте файлы проекта.

---
+++ WpfAppLessonWPF5/MyCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfAppLessonWPF5
+{
+    internal class MyCommand
+    {
+        public static RoutedUICommand Exit { get; set; }
+        static MyCommand()
+        {
+            InputGestureCollection input = new InputGestureCollection();
+            input.Add( new KeyGesture(Key.E, ModifierKeys.Control, "Ctrl+E") );
+            Exit = new RoutedUICommand("Выход", "Exit", typeof(MyCommand), input);
+        }
+    }
+}
